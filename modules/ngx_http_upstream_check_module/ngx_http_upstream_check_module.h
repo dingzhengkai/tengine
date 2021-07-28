038b9f59899c1654bd01193ac59daf4b4b3345e7
Your Name <you@example.com>
2021-07-28 23:10:43 +0800

    fix error: implicit declaration of function

--- modules/ngx_http_upstream_check_module/ngx_http_upstream_check_module.h
+++ modules/ngx_http_upstream_check_module/ngx_http_upstream_check_module.h
@@ -1,1 +1,9 @@
 #define NGX_HAVE_HTTP_UPSTREAM_CHECK
+
+ngx_uint_t ngx_http_upstream_check_add_peer(
+        ngx_conf_t *cf, 
+        ngx_http_upstream_srv_conf_t *us, 
+        ngx_addr_t *peer_addr);
+
+ngx_uint_t ngx_http_upstream_check_peer_down(ngx_uint_t index);
+
